faba85704e422f442c8dedd275caebfbc225b2c1
Ayush Arora <43686887+virtual-ayush-arora@users.noreply.github.com>
2021-04-20 21:03:18 +0530

    Create AddNaturalNumbers.cpp

---
+++ AddNaturalNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long n =0;
+	long result=0;
+	cin >> n;
+	result = (n/2)*(n+1);
+	cout << result;
+	return 0;
+}
